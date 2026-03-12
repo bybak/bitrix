f5a4408f9660d1c5d3888265412f9df7729d63a9
Oleg Maslov <oleg.rikko91@gmail.com>
2026-03-13 01:24:57 +0300

    chore(git): ignore runtime logs/caches
    Stop tracking Bitrix cache, stack cache, nginx logs, and macOS .DS_Store files.
    :

--- www/bitrix/cache/messageservice/b1/b172ba08c82e327a15fe686e8b0692e3.php
+++
@@ -1,7 +0,0 @@
-<?
-if($INCLUDE_FROM_CACHE!='Y')return false;
-$datecreate = '001772172256';
-$dateexpire = '001774764256';
-$ser_content = 'a:2:{s:7:"CONTENT";s:0:"";s:4:"VARS";O:25:"Bitrix\\Main\\Type\\DateTime":2:{s:8:"'.chr(0).'*'.chr(0).'value";O:8:"DateTime":3:{s:4:"date";s:26:"2026-03-29 09:04:16.254458";s:13:"timezone_type";i:3;s:8:"timezone";s:13:"Europe/Moscow";}s:18:"'.chr(0).'*'.chr(0).'userTimeEnabled";b:1;}}';
-return true;
-?>
